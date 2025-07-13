2db2780abee876d8c5b800357e682770fbc541fe
akshatsingh1516 <akshat1516.be23@chitkara.edu.in>
2025-07-13 22:57:48 +0530

    Done labs of enum,guard and optionals

---
+++ 1 - Optionals/Lab - Optionals.playground/Pages/5. Exercise - Failable Initializers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+/*:
+## Exercise - Failable Initializers
+
+ Create a `Computer` struct with two properties, `ram` and `yearManufactured`, where both parameters are of type `Int`. Create a failable initializer that will only create an instance of `Computer` if `ram` is greater than 0, and if `yearManufactured` is greater than 1970, and less than 2020.
+ */
+struct Computer{
+    var ram : Int
+    var yearManufactured : Int
+    
+    init?(ram: Int, yearManufactured: Int){
+        if ram > 0 && yearManufactured > 1970 && yearManufactured < 2020{
+            self.ram = ram
+            self.yearManufactured = yearManufactured
+        }
+        else{
+            return nil
+        }
+    }
+}
+
+//:  Create two instances of `Computer?` using the failable initializer. One instance should use values that will have a value within the optional, and the other should result in `nil`. Use if-let syntax to unwrap each of the `Computer?` objects and print the `ram` and `yearManufactured` if the optional contains a value.
+let computer1 = Computer(ram: 16, yearManufactured: 2019)
+let computer2 = Computer(ram: 32, yearManufactured: 2022)
+
+if let computer1 = computer1{
+    print("Valid Computer: \(computer1.ram)GB RAM, made in \(computer1.yearManufactured)")
+} else {
+    print("Failed to create valid computer.")
+}
+
+if let computer2 = computer2{
+    print("Valid Computer: \(computer2.ram)GB RAM, made in \(computer2.yearManufactured)")
+} else {
+    print("Failed to create valid computer.")
+}
+/*:
+[Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Workout or Nil](@next)
+ */
